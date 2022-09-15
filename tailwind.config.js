21c3bf4b2878e9e9bdc3d9800ff92cf74e00dd99
Mike <U10604@rts-365.dk>
2022-09-15 10:11:23 +0200

    GOT TAILWIND WORKING 1111!!!!!!11111

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {
-      colors: {
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
-        additionalColor: "var(--additional-color)",
-        gradientColor: "var(--gradient-color)",
+    extend: {},
+    colors: {
+      primary: "var(--primary)",
+      secondary: "var(--secondary)",
+      additionalColor: "var(--additional-color)",
+      gradientColor: "var(--gradient-color)",
 
-        darkBlue: "var(--dark-blue",
-        middleBlue: "var(--middle-blue)",
-        lightBlue: "var(--light-blue)",
-        darkGreen: "var(--dark-green)",
-        green: "var(--green)",
-        yellow: "var(--yellow)",
-        orange: "var(--orange)",
-        red: "var(--red)",
-      },
-      fontSize: {
-        headlineOne: "var(--headline-1)",
-        headlineTwo: "var(--headline-2)",
-        headlineThree: "var(--headline-3)",
-        headlineFour: "var(--headline-4)",
-        headlineFive: "var(--headline-5)",
-        headlineSix: "var(--headline-6)",
-      }
+      darkBlue: "var(--dark-blue",
+      middleBlue: "var(--middle-blue)",
+      lightBlue: "var(--light-blue)",
+      darkGreen: "var(--dark-green)",
+      green: "var(--green)",
+      yellow: "var(--yellow)",
+      orange: "var(--orange)",
+      red: "var(--red)",
     },
+    fontSize: {
+      headlineOne: "var(--headline-1)",
+      headlineTwo: "var(--headline-2)",
+      headlineThree: "var(--headline-3)",
+      headlineFour: "var(--headline-4)",
+      headlineFive: "var(--headline-5)",
+      headlineSix: "var(--headline-6)",
+    }
   },
   plugins: [],
 }
