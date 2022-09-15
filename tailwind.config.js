3c34020f87b99746457bc870aa6185cacdd3e00d
Mike <U10604@rts-365.dk>
2022-09-15 09:48:50 +0200

    variables done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        additionalColor: "var(--additional-color)",
+        gradientColor: "var(--gradient-color)",
+
+        darkBlue: "var(--dark-blue",
+        middleBlue: "var(--middle-blue)",
+        lightBlue: "var(--light-blue)",
+        darkGreen: "var(--dark-green)",
+        green: "var(--green)",
+        yellow: "var(--yellow)",
+        orange: "var(--orange)",
+        red: "var(--red)",
+      },
+      fontSize: {
+        headlineOne: "var(--headline-1)",
+        headlineTwo: "var(--headline-2)",
+        headlineThree: "var(--headline-3)",
+        headlineFour: "var(--headline-4)",
+        headlineFive: "var(--headline-5)",
+        headlineSix: "var(--headline-6)",
+      }
+    },
   },
   plugins: [],
 }
