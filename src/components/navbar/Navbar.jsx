d31e5ace2fd4dc58f91dc64e4d4a6e89ff31479a
Mike <U10604@rts-365.dk>
2022-09-26 10:50:04 +0200

    navbar stuff

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
-
+import { PulseOutline, MicOutline, RadioOutline, ContrastOutline, SettingsOutline } from 'react-ionicons'
 const Navbar = () => {
   return (
-    <>
+    <div>
     <footer className=' fixed bottom-0 bg-additionalColor w-full h-12'>
-      <nav>
-
+      <nav className='flex justify-evenly pt-1.5'> 
+      
+      <PulseOutline 
+        color={'rgba(238,9,121,1)'}
+        height="38px"
+        width="38px"
+        title={'pulse'}
+        />
+        <MicOutline
+         color={'rgba(238,9,121,1)'} 
+         title={'mic'}
+         height="38px"
+         width="38px"
+        />
+        <RadioOutline
+        color={'rgba(238,9,121,1)'} 
+        title={'radio'}
+        height="38px"
+        width="38px"
+        />
+        <ContrastOutline
+        color={'rgba(238,9,121,1)'} 
+        title={'darkmode lightmode'}
+        height="38px"
+        width="38px"
+        />
+        <SettingsOutline
+        color={'rgba(238,9,121,1)'} 
+        title={'settings'}
+        height="38px"
+        width="38px"
+        />
       </nav>
     </footer>
-    </>
+    </div>
   )
 }
 
