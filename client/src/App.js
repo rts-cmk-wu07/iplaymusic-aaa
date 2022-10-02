1b4ae25bcae4c8b00244e0a68e6645f247596a2e
Adam <adamchatila2020@gmail.com>
2022-10-02 15:37:08 +0200

    general changing

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Here are we going to build our app</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/components/splash/Splash.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Splash() {
-  return (
-    <div>Splash screen</div>
-  )
-}
-
-export default Splash
