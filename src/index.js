01a00f02d3e5039db03eb541958f58631a72f531
Adam <adamchatila2020@gmail.com>
2022-10-13 10:28:18 +0200

    making soem shits

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router,Routes,Route, Link } from 'react-router-dom';  
+import { store } from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
 );
-
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import { shazamCoreApi } from './services/shazamCore';
 import playerReducer from './features/playerSlice';
-
-import {shazamCoreApi} from './services/shazamCore'
 
 export const store = configureStore({
   reducer: {
-    [shazamCoreApi.reducerPath]:shazamCoreApi.reducer,
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
